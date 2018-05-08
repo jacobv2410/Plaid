4114c12374ea8d5723db9f0879b926a3bbdfa7f7
jodecile84 <jodecile84@gmail.com>
2018-05-07 21:56:33 -0700

    add new user modal, updated login page, bills page, some visual up dates

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -17,3 +17,13 @@ $(document).ready(function () {
   })
   // alert('connected')
 })
+
+/* Calendar ===================================================================================================================== */
+$(document).ready(function() {
+
+  $(".datepicker").datepicker({
+    prevText: '<i class="fa fa-fw fa-angle-left"></i>',
+    nextText: '<i class="fa fa-fw fa-angle-right"></i>'
+  });
+});
+
