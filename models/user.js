e6f1f98237deca143060055a3536ae328240166e
Hoang-Minh <quataoxanh83@gmail.com>
2018-05-11 19:26:26 -0700

    'remove comment

--- models/user.js
+++ models/user.js
@@ -1,26 +1,3 @@
-// //var bcrypt = require("bcrypt");
-
-// module.exports = function (sequelize, DataTypes) {
-//   var User = sequelize.define('User', {
-//     username: DataTypes.STRING,
-//     email: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   }, {
-//     instanceMethods: {
-//       generateHash(password){
-//         return bcrypt.hash(password, bcrypt.genSaltSync(8));
-//       },
-//       validPassword(password){
-//         return bcrypt.compare(password, this.password);
-//       }
-//     }
-//   });
-
-//   return User;
-// }
-
-//var bcrypt = require("bcrypt");
-
 module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
